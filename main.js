2e0bc0c14156e991c23f058cf1502a479542724f
kchatr <cha_kaushik@hotmail.com>
2020-05-23 17:12:30 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// let i = 0;
+// let text = "Software Developer"
+// let speed = 50;
+
+// function typing(){
+//     if(i < text.length){
+//         document.getElementById("type-animation").innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(typing, speed);
+//     }
+// } 
+
